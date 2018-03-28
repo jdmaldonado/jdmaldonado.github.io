5bea8df7dfd9def82858914e482de363cad6ca02
Juan David Maldonado <maldo.s@hotmail.com>
2018-03-28 10:53:32 -0500

    Change name to jmaldonadog, add "how i am" section, add font awesome 5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('jdmaldonado.com App', () => {
+describe('jdmaldonadog.com App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import fontawesome from '@fortawesome/fontawesome';
+import { faChessKnight } from '@fortawesome/fontawesome-free-solid';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor () {
+    fontawesome.library.add(faChessKnight);
+  }
 }
