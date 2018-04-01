1b25e144d9a231b5a4294c0a596a83847f77984c
Juan David Maldonado <maldo.s@hotmail.com>
2018-04-01 10:09:17 -0500

    Add Materialize css as library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MaterializeModule } from 'angular2-materialize';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -20,8 +21,9 @@ export function HttpLoaderFactory(http: HttpClient) {
   ],
   imports: [
     BrowserModule,
-    RoundProgressModule,
     HttpClientModule,
+    MaterializeModule,
+    RoundProgressModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
